3b96ab715e8af3fde99b05305ba12533a90baa06
jayasreeyuvi <jayasreeragou@gmail.com>
2023-04-06 19:56:22 -0300

    initial push

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
 $(function () {
 
-    var currentDay = $("#currentDay");
-    function todisplayTime() {
-      let currentTime = dayjs().format('MMM DD, YYYY hh:mm:ss a');
-      currentDay.text(currentTime);
-    }
+  var currentDay = $("#currentDay");
+  function todisplayTime() {
+    let currentTime = dayjs().format('MMM DD, YYYY hh:mm:ss a');
+    currentDay.text(currentTime);
+  }
     
-    todisplayTime()
-    setInterval(todisplayTime, 1000);
+  todisplayTime()
+  setInterval(todisplayTime, 1000);
+
+  var saveBtn = $(".saveBtn")
+  
+  saveBtn.on("click" , function() {
+   
+    let timeBlock = $(this).parent()
+    let timeBlockId = timeBlock.attr("id")
+    let textArea = timeBlock.find("textarea")
+    let textAreaValue = textArea.val()
+    localStorage.setItem(timeBlockId , textAreaValue)
+    if ( textAreaValue === "" ) {
+      localStorage.removeItem(timeBlockId)
+    }
+  })
+
+    
 
 });
   
